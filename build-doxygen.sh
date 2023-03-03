27c78d00a45638e1014be2fa7ad1c18c3691b927
Rob McKay <robert.mckay@global.komatsu>
2023-03-03 14:13:39 +0000

    Split java out from install line to determine vulnerabilities

--- build-doxygen.sh
+++ build-doxygen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t rmckaykm/doxygen:1.9.6 -f src/Dockerfile-doxygen .
+docker build -t rmckaykm/doxygen:1.9.6-test -f src/Dockerfile-doxygen .
 
-#docker run --rm -it -v $(pwd):/home/build rmckaykm/doxygen:latest apt list --installed
+docker run --rm -it -v $(pwd):/home/build rmckaykm/doxygen:1.9.6-test apt list --installed
